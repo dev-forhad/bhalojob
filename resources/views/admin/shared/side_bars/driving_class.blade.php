c1fea7da8073ecc1f775a72bc412bc06eda0bb1c
khanforhad <dev.forhadkhan@gmail.com>
2024-03-10 18:44:52 +0900

    delete file

--- resources/views/admin/shared/side_bars/driving_class.blade.php
+++
@@ -1,8 +0,0 @@
-<li class="nav-item  "> <a href="javascript:;" class="nav-link nav-toggle"> <i class="fa fa-language" aria-hidden="true"></i> <span class="title">Driving Certificate</span> <span class="arrow"></span> </a>
-    <ul class="sub-menu">
-        <li class="nav-item  "> <a href="{{ route('list.drivingclass') }}" class="nav-link "> <span class="title">List Class</span> </a> </li>
-        <li class="nav-item  "> <a href="{{ route('create.drivingclass') }}" class="nav-link "> <span class="title">Add new Class</span> </a> </li>
-        <li class="nav-item  "> <a href="{{ route('list.driving.category') }}" class="nav-link "> <span class="title">List Category</span> </a> </li>
-        <li class="nav-item  "> <a href="{{ route('create.driving.category') }}" class="nav-link "> <span class="title">Add new Category</span> </a> </li>
-    </ul>
-</li>
--- resources/views/admin/shared/side_bars/language_type.blade.php
+++
@@ -1,6 +0,0 @@
-<li class="nav-item  "> <a href="javascript:;" class="nav-link nav-toggle"> <i class="fa fa-bar-chart" aria-hidden="true"></i> <span class="title">Language Type</span> <span class="arrow"></span> </a>
-    <ul class="sub-menu">
-        <li class="nav-item  "> <a href="{{ route('list.language.types') }}" class="nav-link "> <span class="title">List Language Type</span> </a> </li>
-        <li class="nav-item  "> <a href="{{ route('create.language.types') }}" class="nav-link ">  <span class="title">Add new Language Type</span> </a> </li>
-    </ul>
-</li>
